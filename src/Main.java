4f36aaf960000996a09d5c319c192f1fb7b1c6be
rogerwerle <mwerleroger@gmail.com>
2025-06-03 19:35:13 -0300

    criacao do form e exibicao no terminal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        File file = new File ("form.txt");
+        try (FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)) {
 
+            String linha;
+            while((linha = br.readLine()) != null) {
+                System.out.println(linha);
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
